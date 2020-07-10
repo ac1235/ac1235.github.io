cc33665bb4a35eca9a2b6d193562281d2e099aa2
Anthony Cipriano <anthony1235@mail.ru>
2020-07-11 01:30:04 +0200

    init

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+
+        // Make Page Title
+        document.title = 'Anthony Cipriano';
+
+        // Make Header
+        var header = document.createElement('span');
+        header.innerHTML = '<strong><a href="index.html" class="hiddenlink">Anthony Cipriano</a></strong> (<a href="https://twitter.com/pldanthony">@pldanthony</a>)';
+        document.body.insertBefore(header, document.body.firstChild);
+
+};
